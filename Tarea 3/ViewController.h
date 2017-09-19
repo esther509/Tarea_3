8c18ba4aaab1527ad74c8f54e8271d32928f22b0
Laura Esther Lopez <esther_509@hotmail.com>
2017-09-19 00:46:37 -0500

    Image integrated with sliders RGB

--- Tarea 3/ViewController.h
+++ Tarea 3/ViewController.h
@@ -7,9 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#define RGB(r, g, b) [UIColor colorWithRed:(float)r / 255.0 green:(float)g / 255.0 blue:(float)b / 255.0 alpha:1.0]
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIImageView *circleImage;
+@property (weak, nonatomic) IBOutlet UISlider *sliderRedValue;
+@property (weak, nonatomic) IBOutlet UISlider *sliderGreenValue;
+@property (weak, nonatomic) IBOutlet UISlider *sliderBlueValue;
+
+@property float redValue;
+@property float greenValue;
+@property float blueValue;
+
+- (void) changeColor;
 
 @end
 
--- Tarea 3/ViewController.m
+++ Tarea 3/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.redValue = 127.5;
+    self.greenValue = 127.5;
+    self.blueValue = 127.5;
+   
 }
 
 
@@ -24,6 +28,38 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)changeColor {
+    self.circleImage.tintColor = RGB(self.redValue, self.greenValue, self.blueValue);
+     //self.circleImage.tintColorDidChange();
+}
+
+- (IBAction)onColorSliderRed:(id)sender {
+    self.redValue = self.sliderRedValue.value;
+    
+    UIColor *color = RGB(self.redValue, self.greenValue, self.blueValue);
+    
+    //self.changeColor();
+    self.circleImage.image = [self.circleImage.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    [self.circleImage setTintColor:color];
+    
+    //self.circleImage.tintColor = RGB(self.redValue, self.greenValue, self.blueValue);
+}
+
+- (IBAction)onColorSliderGreen:(id)sender {
+    self.greenValue = self.sliderGreenValue.value;
+    
+    //self.colorChanged();
+    self.circleImage.backgroundColor = RGB(self.redValue, self.greenValue, self.blueValue);
+}
+
+- (IBAction)onColorSliderBlue:(id)sender {
+    self.blueValue = self.sliderBlueValue.value;
+    
+    //self.colorChanged();
+    self.circleImage.backgroundColor = RGB(self.redValue, self.greenValue, self.blueValue);
+}
+
 
 
 @end
